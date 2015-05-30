3519aeca2544301c2ce99018a289dc94b31e4936
Bozidar <bozidar.labas@hotmail.com>
2015-05-30 22:07:07 +0200

    Telescoping Constructor pattern.

---
+++ TelescopingConstructorPattern/src/main/java/com/bozidar/effectivejava/App.java
@@ -0,0 +1,11 @@
+package com.bozidar.effectivejava;
+
+/**
+ * Created by Bozidar on 30.5.2015..
+ */
+public class App {
+
+    public static void main(String[] args){
+        NutritionFacts cocaCola = new NutritionFacts(240, 8, 100, 0, 35, 27);
+    }
+}
